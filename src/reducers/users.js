06791a34d10444e846e68b7a23b41c8e30b05459
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2021-12-10 23:12:09 +0200

    fixed an issue with add user action

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,10 +1,18 @@
-import { ANSWER_QUESTION } from "../actions/questions";
+import { ADD_QUESTION, ANSWER_QUESTION } from "../actions/questions";
 import { ADD_USER, GET_USERS } from "../actions/users";
 
 export const users = (state = [], action) => {
     switch (action.type) {
         case GET_USERS:
             return action.users
+        case ADD_QUESTION:
+            return {
+                ...state,
+                [action.question.author]: {
+                    ...state[action.question.author],
+                    questions: state[action.question.author].questions.concat([action.authedUser])
+                }
+            }
         case ANSWER_QUESTION:
             return {
                 ...state,
